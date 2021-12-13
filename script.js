e2e4b0cc97dacf7a1679f3f9e4c510bf762fa26c
arcqkk5 <arcq-k5@yandex.ru>
2021-12-13 10:56:50 +0300

    change js

--- script.js
+++ script.js
@@ -33,3 +33,77 @@ document.addEventListener('keydown', function (e) {
     closeModalWindow();
   }
 });
+
+//////////////////////////////////////
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+console.log(document.querySelector('.header'));
+const sections = document.querySelectorAll('.section');
+console.log(sections);
+
+console.log(document.getElementById('section--1'));
+console.log(document.querySelector('#section--1'));
+
+const buttons = document.getElementsByTagName('button');
+console.log(buttons);
+
+const getButton = document.getElementsByClassName('btn');
+console.log(getButton);
+
+///////////////////////////////////////
+const message = document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'Мы используем на этом сайте cookie для улучшения функциональности';
+message.innerHTML =
+  'Мы используем на этом сайте cookie для улучшния его функциональности. <button class="btn btn--close-cookie">OK!</button>';
+const header = document.querySelector('.header');
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true));
+
+// header.before(message);
+// header.after(message);
+
+document
+  .querySelector('.btn--close-cookie')
+  .addEventListener('click', function () {
+    message.remove();
+    //old
+    // message.parentElement.replaceChild(message);
+  });
+
+/////////////////////////////////////
+message.style.backgroundColor = '#076785';
+message.style.width = '100%';
+
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message));
+console.log(getComputedStyle(message).color);
+
+// console.log(message.style.height);
+console.log(getComputedStyle(message).height); //43
+message.style.height =
+  Number.parseFloat(getComputedStyle(message).height) + 30 + 'px';
+console.log(message.style.height); //93
+///////////////////////////////////////
+// document.documentElement.style.setProperty('--color-first', 'yellow');
+
+///////////////////////////////////////
+//atribut
+const logo = document.querySelector('.nav__logo');
+console.log(logo.alt);
+console.log(logo.src);
+console.log(logo.className);
+console.log(logo.getAttribute('dev'));
+logo.setAttribute('copy', 'no');
+
+//////////////////////////////////////
+logo.classList.add('a', 'b');
+logo.classList.remove('a');
+logo.classList.toggle('a');
+logo.classList.contains('c');
+
+/////////NOOOO////////////////
+logo.className = 'a'; // Перезатерает все названия классов
